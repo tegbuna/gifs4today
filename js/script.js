8588964e53c7afe35c6e88c2caa61052eab9b174
Tasha Egbuna <tasha.egbuna@gmail.com>
2021-03-01 20:54:37 -0600

    updates to inital draft of html and endpoint tests

--- js/script.js
+++ js/script.js
@@ -4,20 +4,33 @@
 
 //BASE_URL3 w/ Search Endpoint: 'https://api.giphy.com/v1/gifs/search?api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0'
 
-// Key one: qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx
+// Top Ten Key: qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx
 
-// Key two: ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS
+// Trending Searches Key: ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS
 
-//Key three: TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0
+//Search Key: TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0
 
-var top10 = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx");
-top10.done(function(data) { console.log("success got data", data); });
+//API Top 10 Endpiont Test:
+const top10 = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx");
+top10.done(function(data) { console.log("success got data", data); 
+}, (error) =>{
+  console.log('bad request', error);
+});
 
-var trends = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS");
-trends.done(function(data) { console.log("success got data", data); });
+//API Trending Searches Endpiont Test:
+const trends = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS");
+trends.done(function(data) { console.log("success got data", data); 
+}, (error) =>{
+  console.log('bad request', error);
+});
+
+//API Search Endpiont Test:
+const srch = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0&limit=5");
+srch.done(function(data) { console.log("success got data", data); 
+}, (error) =>{
+  console.log('bad request', error);
+});
 
 
-var srch = $.get("http://api.giphy.com/v1/gifs/search?q=&api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0&limit=5");
-srch.done(function(data) { console.log("success got data", data); });
 
 
