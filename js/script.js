24991de144f0ca75f13651eaaf5ee5f2b2c21221
Tasha Egbuna <tasha.egbuna@gmail.com>
2021-03-03 12:26:26 -0600

    updates to css, js and index files - api is populaiting key terms and top10 gifs

--- js/script.js
+++ js/script.js
@@ -51,15 +51,39 @@
 //   });
 
 //Get Popular Search Key Terms List:
-function formatList(data) {
-    $.ajax({
-    
-        url: "http://api.giphy.com/v1/gifs/search?q="  +  "&api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS&limit=18",
-        type: "GET",
-         function (data) {
-                $('.col-lg-4p').text(`<p> ${data.data[0]} </p>`);
-            }, (error) => {
-              console.log('bad request: ', error);
-            }); 
-        })
-    };
+$(document).ready(formatList);
+function formatList(e) {
+
+    $.ajax('https://api.giphy.com/v1/trending/searches?api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS&limit=27').then(function(data1){
+        $('#key-term-1').text(data1.data[0])
+        $('#key-term-2').text(data1.data[1])
+        $('#key-term-3').text(data1.data[2])
+        $('#key-term-4').text(data1.data[3])
+        $('#key-term-5').text(data1.data[4])
+        $('#key-term-6').text(data1.data[5])
+        $('#key-term-7').text(data1.data[6])
+        $('#key-term-8').text(data1.data[7])
+        $('#key-term-9').text(data1.data[8])
+        $('#key-term-10').text(data1.data[9])
+        $('#key-term-11').text(data1.data[10])
+        $('#key-term-12').text(data1.data[11])
+        $('#key-term-13').text(data1.data[12])
+        $('#key-term-14').text(data1.data[13])
+        $('#key-term-15').text(data1.data[14])
+        $('#key-term-16').text(data1.data[15])
+        $('#key-term-17').text(data1.data[16])
+        $('#key-term-18').text(data1.data[17])
+    })
+    $.ajax('https://api.giphy.com/v1/gifs/trending?api_key=qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx&limit=10').then (function(data2){
+        $('#gif10').attr('src', data2.data[0].images.original.url);
+        $('#gif9').attr('src', data2.data[1].images.original.url);
+        $('#gif8').attr('src', data2.data[2].images.original.url);
+        $('#gif7').attr('src', data2.data[3].images.original.url);
+        $('#gif6').attr('src', data2.data[4].images.original.url);
+        $('#gif5').attr('src', data2.data[5].images.original.url);
+        $('#gif4').attr('src', data2.data[6].images.original.url);
+        $('#gif3').attr('src', data2.data[7].images.original.url);
+        $('#gif2').attr('src', data2.data[8].images.original.url);
+        $('#gif1').attr('src', data2.data[9].images.original.url);
+    })
+}
