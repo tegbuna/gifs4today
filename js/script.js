f1b00262ebddcd68f95910c5eaa2026f2386d845
Tasha Egbuna <tasha.egbuna@gmail.com>
2021-03-03 12:38:20 -0600

    clean up js

--- js/script.js
+++ js/script.js
@@ -1,55 +1,18 @@
 
 
 
-//IPO Pattern for program design - Input -> Process -> Input
-
 // BASE_URL w/ Top 10 Endpoint: 'https://api.giphy.com/v1/gifs/trending?api_key=qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx&limit=10'
-
-//BASE_URL2 w/ Trending Search Terms Endpoint: 'https://api.giphy.com/v1/trending/searches?api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS&limit=27'
-
-//BASE_URL3 w/ Search Endpoint: 'https://api.giphy.com/v1/gifs/search?api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0'
-
 // Top Ten Key: qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx
 
+//BASE_URL2 w/ Trending Search Terms Endpoint: 'https://api.giphy.com/v1/trending/searches?api_key=ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS&limit=27'
 // Trending Searches Key: ca1ep15FwAhmLD1t2cAUus1ubyOp2IsS
 
+//BASE_URL3 w/ Search Endpoint: 'https://api.giphy.com/v1/gifs/search?api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0'
 //Search Key: TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0
 
 
-/*----- constants -----*/
 
-
-/*----- app's state (variables) -----*/
-
-
-/*----- cached element references -----*/
-// const $toptrends = $('#toptrends');
-// const $gifs = $('#gif');
-// const $modal = $('modal');
-// const $form = $('#form')
-
-/*----- event listeners -----*/
-// $('.btn-outline-success').click(function(e)
-
-
-/*----- functions -----*/
-//For Search Items (needs modal):
-// $('.btn-outline-success').click(function(e) { 
-//   e.preventDefault()
-//       console.log("The button was clicked.")
-  
-//   $.ajax({
-  
-//       url: "http://api.giphy.com/v1/gifs/search?q=" + $('.form-control me-2').val() +  "&api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0",
-//       type: "GET",
-//       success: function(data) {
-//           console.log("This works too")
-//           debugger
-//           console.log(response.data[0].bitly_url);
-//       }
-//   });
-//   });
-
+/*----- FUNCTIONS -----*/
 //Get Popular Search Key Terms List:
 $(document).ready(formatList);
 function formatList(e) {
@@ -74,6 +37,8 @@ function formatList(e) {
         $('#key-term-17').text(data1.data[16])
         $('#key-term-18').text(data1.data[17])
     })
+// Get Top 10 GIFs of the day:
+
     $.ajax('https://api.giphy.com/v1/gifs/trending?api_key=qb1xRZmU2B2eMGsiO78pCPnk1XOM0Rgx&limit=10').then (function(data2){
         $('#gif10').attr('src', data2.data[0].images.original.url);
         $('#gif9').attr('src', data2.data[1].images.original.url);
@@ -87,3 +52,20 @@ function formatList(e) {
         $('#gif1').attr('src', data2.data[9].images.original.url);
     })
 }
+
+//For Search Items (needs modal):
+// $('.btn-outline-success').click(function(e) { 
+//   e.preventDefault()
+//       console.log("The button was clicked.")
+  
+//   $.ajax({
+  
+//       url: "http://api.giphy.com/v1/gifs/search?q=" + $('.form-control me-2').val() +  "&api_key=TtNU4DZGqrYC7rkkWQckj4PC8tlwv2z0",
+//       type: "GET",
+//       success: function(data) {
+//           console.log("This works too")
+//           debugger
+//           console.log(response.data[0].bitly_url);
+//       }
+//   });
+//   });
